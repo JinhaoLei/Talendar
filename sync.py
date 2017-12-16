a1b2ee830a286d3f307b9b4f9c484add9fab06f4
jia9110 <jia9110@163.com>
2017-12-16 15:25:38 +0800

    Update sync.py

--- sync.py
+++ sync.py
@@ -218,8 +218,8 @@ if __name__ == '__main__':
 	username=''
 	password=''  # TODO:change username and password
 	if username is None or password is None:
-        username = input("TsinghuaId:")
-        password = getpass.getpass("Password:")
+        	username = input("TsinghuaId:")
+        	password = getpass.getpass("Password:")
 	print(json.dumps(WebLearningScraper(username, password).courses(),ensure_ascii=False,indent = 4))
 	print(json.dumps(GetTimetable(username, password).get_course_time(),ensure_ascii=False,indent = 4))
 
